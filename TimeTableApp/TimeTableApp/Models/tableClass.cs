8b3d4bec19e9daac63a14388a55610a3283fc1f6
Thomas <s00119500@mail.itsligo.ie>
2014-04-21 01:50:49 +0100

    start (MVC)
    started with some basic classes

---
+++ TimeTableApp/TimeTableApp/Models/tableClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeTableApp.Models
+{
+    public class tableClass
+    {
+    }
+
+    public class students
+    {
+        public int studentId { get; set; }
+        public string firstName { get; set; }
+        
+    }
+
+    public class courses
+    {
+        public int courseId { get; set; }
+        public string courseName { get; set; }
+
+    }
+
+    public class rooms
+    {
+        public int roomId { get; set; }
+        public string roomName { get; set; }
+    }
+
+
+
+}
